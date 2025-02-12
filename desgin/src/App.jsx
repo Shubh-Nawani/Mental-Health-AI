31e3dde0d066cdeebdccf4f9b916709da0733ab2
Aryaan007-sudo <blankgaming75@gmail.com>
2025-02-12 19:43:42 +0530

    design

---
+++ desgin/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import LandingPage from './LandingPage'
+import HomeCare from './HomeCare'
+import Login from './LoginPage';
+
+
+function App() {
+  return (
+      <div className='Landingpage'>
+        <LandingPage></LandingPage>
+        <HomeCare></HomeCare>
+      </div>
+      
+      
+  )
+}
+
+export default App
